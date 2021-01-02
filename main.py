422a613e879c9d53abbd5625a4b5a1e49a206099
DesolateSoul <con1337@yandex.ru>
2021-01-02 17:48:29 +0700

    Main character now can break platforms

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from pygame import *
 import pygame
 import pyganim
 import random
+import datetime
 
 WINDOW_WIDTH = 1400
 WINDOW_HEIGHT = 700
@@ -19,12 +20,18 @@ GRAVITY = 0.7
 PLATFORM_WIDTH = 64
 PLATFORM_HEIGHT = 64
 
-ANIMATION_RIGHT = [('data/run1.png', 0.1), ('data/run2.png', 0.1), ('data/run3.png', 0.1), ('data/run4.png', 0.1),
-                   ('data/run5.png', 0.1), ('data/run6.png', 0.1)]
-ANIMATION_JUMP_RIGHT = [('data/jump1.png', 0.2), ('data/jump2.png', 0.2), ('data/jump3.png', 0.2),
-                        ('data/jump4.png', 0.2), ('data/jump5.png', 0.2), ('data/jump6.png', 0.2)]
-ANIMATION_STAY_RIGHT = [('data/idle1.png', 0.2), ('data/idle2.png', 0.2), ('data/idle3.png', 0.2),
-                        ('data/idle4.png', 0.2)]
+ANIMATION_RUN = [('data/run1.png', 0.1), ('data/run2.png', 0.1), ('data/run3.png', 0.1), ('data/run4.png', 0.1),
+                 ('data/run5.png', 0.1), ('data/run6.png', 0.1)]
+ANIMATION_JUMP = [('data/jump1.png', 0.2), ('data/jump2.png', 0.2), ('data/jump3.png', 0.2),
+                  ('data/jump4.png', 0.2), ('data/jump5.png', 0.2), ('data/jump6.png', 0.2)]
+ANIMATION_STAY = [('data/idle1.png', 0.2), ('data/idle2.png', 0.2), ('data/idle3.png', 0.2),
+                  ('data/idle4.png', 0.2)]
+ANIMATION_ATTACK1 = [('data/attack_1_1.png', 0.1), ('data/attack_1_2.png', 0.1), ('data/attack_1_3.png', 0.1),
+                     ('data/attack_1_4.png', 0.1), ('data/attack_1_5.png', 0.1), ('data/attack_1_6.png', 0.1)]
+ANIMATION_ATTACK1_LEFT = [('data/attack_1_1_left.png', 0.1), ('data/attack_1_2_left.png', 0.1),
+                          ('data/attack_1_3_left.png', 0.1), ('data/attack_1_4_left.png', 0.1),
+                          ('data/attack_1_5_left.png', 0.1), ('data/attack_1_6_left.png', 0.1)]
+ANIMATION_TIME = datetime.timedelta(milliseconds=600)
 
 
 class Player(sprite.Sprite):
@@ -42,27 +49,35 @@ class Player(sprite.Sprite):
 
         self.attack = attack
         self.gaze_direction = gaze_direction  # Направление взгляда
-        self.image = Surface((WIDTH, HEIGHT))
+        self.image = Surface((76, 76))
         self.image.fill(Color(COLOR))
         self.rect = Rect(x, y, WIDTH, HEIGHT)  # прямоугольный объект
         self.image.set_colorkey(Color(COLOR))  # делаем фон прозрачным
         #  Анимация движения вправо
-        self.animation_run = pyganim.PygAnimation(ANIMATION_RIGHT)
+        self.animation_run = pyganim.PygAnimation(ANIMATION_RUN)
         self.animation_run.play()
         #  Анимация бездействия
-        self.animation_stay = pyganim.PygAnimation(ANIMATION_STAY_RIGHT)
+        self.animation_stay = pyganim.PygAnimation(ANIMATION_STAY)
         self.animation_stay.play()
         #  Анимация прыжка вправо
-        self.animation_jump = pyganim.PygAnimation(ANIMATION_JUMP_RIGHT)
+        self.animation_jump = pyganim.PygAnimation(ANIMATION_JUMP)
         self.animation_jump.play()
+
+        self.animation_attack1 = pyganim.PygAnimation(ANIMATION_ATTACK1)
+        self.animation_attack1.play()
+
+        self.animation_attack1_left = pyganim.PygAnimation(ANIMATION_ATTACK1_LEFT)
+        self.animation_attack1_left.play()
 
         self.animation_run.scale((WIDTH, HEIGHT))
         self.animation_stay.scale((WIDTH, HEIGHT))
         self.animation_jump.scale((WIDTH, HEIGHT))
+        self.animation_attack1.scale((76, 76))
+        self.animation_attack1_left.scale((76, 76))
 
         self.animation_stay.blit(self.image, (0, 0))  # По-умолчанию, стоим
 
-    def update(self, left, right, up, attack, platforms):
+    def update(self, left, right, up, attack, platforms, time_was=datetime.datetime.now()):
         if up:
             if self.ground_touch:  # Прыгаем, только когда можем оттолкнуться от земли
                 self.y_speed = -JUMP_POWER
@@ -84,10 +99,14 @@ class Player(sprite.Sprite):
                 self.animation_run.blit(self.image, (0, 0))
         if not (left or right):  # Стоим, когда нет указаний идти
             self.x_speed = 0
-            #  if attack:
-            #  self.image.fill(Color(COLOR))
-            #  self.animation_attack1.blit(self.image, (0, 0))
-            if not up and self.ground_touch:
+            if attack:
+                if self.gaze_direction == "right":
+                    self.image.fill(Color(COLOR))
+                    self.animation_attack1.blit(self.image, (0, 0))
+                elif self.gaze_direction == "left":
+                    self.image.fill(Color(COLOR))
+                    self.animation_attack1_left.blit(self.image, (0, 0))
+            elif not up and self.ground_touch:
                 self.image.fill(Color(COLOR))
                 self.animation_stay.blit(self.image, (0, 0))
         if not self.ground_touch:  # изменение скорости в зависимости от гравитации
@@ -95,11 +114,21 @@ class Player(sprite.Sprite):
 
         self.ground_touch = False
         self.rect.y += self.y_speed  # Изменение положения квадрата игрока
-        self.collide(0, self.y_speed, platforms)  # Проверка столкновений
+        self.collide(0, self.y_speed, platforms, time_was)  # Проверка столкновений
         self.rect.x += self.x_speed  # Изменение положения квадрата игрока
-        self.collide(self.x_speed, 0, platforms)  # Проверка столкновений
+        self.collide(self.x_speed, 0, platforms, time_was, self.attack)  # Проверка столкновений
 
-    def collide(self, x_speed, y_speed, platforms):
+    def can_attack(self, platform):
+        if self.rect.x + WIDTH == platform.rect.x and self.rect.y == platform.rect.y \
+                and self.gaze_direction == "right":
+            return True
+        elif self.rect.x == platform.rect.x + PLATFORM_WIDTH and self.rect.y == platform.rect.y \
+                and self.gaze_direction == "left":
+            return True
+        else:
+            return False
+
+    def collide(self, x_speed, y_speed, platforms, time_was, attack=False):
         for platform in platforms:
             if sprite.collide_rect(self, platform):
                 if x_speed > 0:
@@ -113,6 +142,8 @@ class Player(sprite.Sprite):
                 if y_speed < 0:
                     self.rect.top = platform.rect.bottom
                     self.y_speed = 0
+            if self.can_attack(platform) and attack:
+                platform.update_condition(platforms, time_was)
 
 
 class Camera:
@@ -163,12 +194,29 @@ def generate_level(filename):
 
 
 class Platform(sprite.Sprite):
-    def __init__(self, x, y):
+    def __init__(self, x, y, condition=1):
         sprite.Sprite.__init__(self)
         self.image = Surface((PLATFORM_WIDTH, PLATFORM_HEIGHT))
         self.image = image.load("data/platform.png")
         self.image = pygame.transform.scale(self.image, (64, 64))
         self.rect = Rect(x, y, PLATFORM_WIDTH, PLATFORM_HEIGHT)
+        self.condition = condition
+        self.time_was = datetime.datetime.now()
+
+    def update_condition(self, platforms, time_was):
+        if datetime.datetime.now() - self.time_was >= ANIMATION_TIME:
+            if self.condition == 1:
+                self.time_was = time_was
+            self.condition += 1
+            if self.condition == 2:
+                self.time_was = datetime.datetime.now()
+            if self.condition == 3:
+                self.image = image.load("data/platform_broken.png")
+                self.image = pygame.transform.scale(self.image, (64, 64))
+                self.time_was = datetime.datetime.now()
+            elif self.condition == 4:
+                self.kill()
+                del platforms[platforms.index(self)]
 
 
 def main():
@@ -178,7 +226,6 @@ def main():
     bg = image.load("data/background.png")
     bg = pygame.transform.scale(bg, (4096, 2048))
     timer = pygame.time.Clock()
-
     main_character = Player(64, 1920)
     objects = pygame.sprite.Group()  # Все объекты
     objects.add(main_character)
@@ -200,7 +247,6 @@ def main():
     total_level_width = len(level[0]) * PLATFORM_WIDTH  # Получившаяся длина уровня,
     total_level_height = len(level) * PLATFORM_HEIGHT  # высота
     camera = Camera(camera_configure, total_level_width, total_level_height)
-
     running = True
     while running:  # Основной игровой цикл
         gaze_before = main_character.gaze_direction
@@ -230,6 +276,7 @@ def main():
 
             if ev3nt.type == KEYDOWN and ev3nt.key == 122:
                 main_character.attack = True
+                time_now = datetime.datetime.now()
             if ev3nt.type == KEYUP and ev3nt.key == 122:
                 main_character.attack = False
 
@@ -238,9 +285,12 @@ def main():
             main_character.animation_run.flip(True, False)
             main_character.animation_stay.flip(True, False)
             main_character.animation_jump.flip(True, False)
-
-        main_character.update(main_character.move_left, main_character.move_right, main_character.move_up,
-                              main_character.attack, platforms)
+        if main_character.attack:
+            main_character.update(main_character.move_left, main_character.move_right, main_character.move_up,
+                                  main_character.attack, platforms, time_now)
+        else:
+            main_character.update(main_character.move_left, main_character.move_right, main_character.move_up,
+                                  main_character.attack, platforms)
         camera.update(main_character)
         for obj in objects:
             screen.blit(obj.image, camera.apply(obj))
